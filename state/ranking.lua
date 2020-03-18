db4ba5d5ee990c6a1765285e1f10ecca43c06ef6
agdf_ydy <hakka.ydy.sub@gmail.com>
2020-03-18 16:55:08 +0900

    a

--- state/ranking.lua
+++ state/ranking.lua
@@ -18,6 +18,19 @@ function ranking:init()
             }
         }
     )
+    self.joystickManager = JoystickManager()
+    self.joystickManager:register(
+        {
+            {
+                key = 'a',
+                func = function()
+                    self:pressZ()
+                end,
+                rep = false,
+                act = 'pressed'
+            }
+        }
+    )
 end
 
 function ranking:enter()
@@ -41,8 +54,8 @@ function ranking:enter()
 
     scores = {
         x1 = -200,
-        x2 = 1024+200,
-        x = {-200,1024+200,-200,1024+200,-200}
+        x2 = 1024 + 200,
+        x = {-200, 1024 + 200, -200, 1024 + 200, -200}
     }
 
     back = {
@@ -78,26 +91,26 @@ function ranking:enter()
         x = 512,
         y = 132
     }
-    
-    back.cnt = 0
-    flux.to(bigLabel,EASE_TIME,{y=30+64,sx = 20}):ease("expoinout")
-    flux.to(bigLabel,EASE_TIME*0.8,{sy=2}):ease("elasticout")
-    flux.to(scores,EASE_TIME,{x1 = 512, x2 = 512}):ease("expoinout")
 
-    for i=1, #scores.x do
+    back.cnt = 0
+    flux.to(bigLabel, EASE_TIME, {y = 30 + 64, sx = 20}):ease('expoinout')
+    flux.to(bigLabel, EASE_TIME * 0.8, {sy = 2}):ease('elasticout')
+    flux.to(scores, EASE_TIME, {x1 = 512, x2 = 512}):ease('expoinout')
+
+    for i = 1, #scores.x do
         if i ~= 5 then
-            flux.to(scores.x,EASE_TIME,{[6-i]=512}):ease("expoinout"):delay((i)*0.12)
+            flux.to(scores.x, EASE_TIME, {[6 - i] = 512}):ease('expoinout'):delay((i) * 0.12)
         else
-            flux.to(scores.x,EASE_TIME,{[6-i]=512}):ease("expoinout"):delay((i)*0.12+0.15)
+            flux.to(scores.x, EASE_TIME, {[6 - i] = 512}):ease('expoinout'):delay((i) * 0.12 + 0.15)
         end
     end
 
-    flux.to(back,EASE_TIME,{y = 460+32}):ease("expoinout")
-    flux.to(start,EASE_TIME,{x = -150}):ease("expoinout")
-    flux.to(setting,EASE_TIME,{x = 1024+150}):ease("expoinout")
-    flux.to(hanko,EASE_TIME,{x = -150}):ease("expoinout")
-    flux.to(onigo,EASE_TIME,{x = 1024+150}):ease("expoinout")
-    flux.to(cursor,EASE_TIME,{x=750+16*7, y = 460+32}):ease("expoinout")
+    flux.to(back, EASE_TIME, {y = 460 + 32}):ease('expoinout')
+    flux.to(start, EASE_TIME, {x = -150}):ease('expoinout')
+    flux.to(setting, EASE_TIME, {x = 1024 + 150}):ease('expoinout')
+    flux.to(hanko, EASE_TIME, {x = -150}):ease('expoinout')
+    flux.to(onigo, EASE_TIME, {x = 1024 + 150}):ease('expoinout')
+    flux.to(cursor, EASE_TIME, {x = 750 + 16 * 7, y = 460 + 32}):ease('expoinout')
 
     back.cnt = 0
     flux.to(bigLabel, EASE_TIME, {y = 30 + 64, sx = 20}):ease('expoinout')
@@ -114,6 +127,7 @@ end
 function ranking:update(dt)
     flux.update(dt)
     self.keyManager:update(dt)
+    self.joystickManager:update(dt)
 
     if back.cnt > 1 then
         back.cnt = back.cnt + 1
@@ -127,22 +141,21 @@ function ranking:draw()
     love.graphics.setColor(1, 1, 1, 1)
     --love.graphics.setFont(Data.Font.title)
     --love.graphics.print("HANKO ONIGO", 512-5.5*32, 150)
-    self:printCenter(Data.Font.veryBig,Data.Font.size.veryBig,"HANKO      ", hanko.x, hanko.y,1)
-    self:printCenter(Data.Font.veryBig,Data.Font.size.veryBig,"      ONIGO", onigo.x, onigo.y,1)
-    
-    self:printCenter(Data.Font.title,Data.Font.size.title,"RANKING", bigLabel.x, bigLabel.y,bigLabel.sy)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"1st "..scoreValues[1].."pt", scores.x[1], 180+32,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"2nd "..scoreValues[2].."pt", scores.x[2], 250+32,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"3rt "..scoreValues[3].."pt", scores.x[3], 320+32,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"4th "..scoreValues[4].."pt", scores.x[4], 390+32,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"5th "..scoreValues[5].."pt", scores.x[5], 460+32,1)
+    self:printCenter(Data.Font.veryBig, Data.Font.size.veryBig, 'HANKO      ', hanko.x, hanko.y, 1)
+    self:printCenter(Data.Font.veryBig, Data.Font.size.veryBig, '      ONIGO', onigo.x, onigo.y, 1)
 
-    self:printCenter(Data.Font.title,Data.Font.size.title,cursor.label, cursor.x, cursor.y,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,back.moji, back.x, back.y,back.scale)
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'RANKING', bigLabel.x, bigLabel.y, bigLabel.sy)
+    self:printCenter(Data.Font.title, Data.Font.size.title, '1st ' .. scoreValues[1] .. 'pt', scores.x[1], 180 + 32, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, '2nd ' .. scoreValues[2] .. 'pt', scores.x[2], 250 + 32, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, '3rt ' .. scoreValues[3] .. 'pt', scores.x[3], 320 + 32, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, '4th ' .. scoreValues[4] .. 'pt', scores.x[4], 390 + 32, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, '5th ' .. scoreValues[5] .. 'pt', scores.x[5], 460 + 32, 1)
 
-    self:printCenter(Data.Font.title,Data.Font.size.title,"START", start.x, start.y,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"SETTING", setting.x, setting.y,1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, cursor.label, cursor.x, cursor.y, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, back.moji, back.x, back.y, back.scale)
 
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'START', start.x, start.y, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'SETTING', setting.x, setting.y, 1)
 end
 
 function ranking:leave()
@@ -151,20 +164,20 @@ end
 function ranking:pressZ()
     back.cnt = back.cnt + 1
     if back.cnt == 2 then
-        flux.to(bigLabel,EASE_TIME,{y=386,sx = 20,sy=1}):ease("expoinout")
-        flux.to(back,EASE_TIME*0.4,{scale = 1.3}):ease("elasticout")
-        flux.to(back,EASE_TIME*0.3,{scale = 0}):ease("circin"):delay(EASE_TIME*0.4)
-        flux.to(cursor,EASE_TIME,{x=512,y=386}):ease("expoinout")
-        cursor.label=">         <"
-        flux.to(start,EASE_TIME,{x = 512}):ease("expoinout")
-        flux.to(setting,EASE_TIME,{x = 512}):ease("expoinout")
-        flux.to(hanko,EASE_TIME,{x = 512}):ease("expoinout")
-        flux.to(onigo,EASE_TIME,{x = 512}):ease("expoinout")
-    
-        for i=1, #scores.x do
-            flux.to(scores.x,EASE_TIME*0.8,{[i]=1024+200-(1024+400)*(i % 2)}):ease("expoinout"):delay((i)*0.03)
+        flux.to(bigLabel, EASE_TIME, {y = 386, sx = 20, sy = 1}):ease('expoinout')
+        flux.to(back, EASE_TIME * 0.4, {scale = 1.3}):ease('elasticout')
+        flux.to(back, EASE_TIME * 0.3, {scale = 0}):ease('circin'):delay(EASE_TIME * 0.4)
+        flux.to(cursor, EASE_TIME, {x = 512, y = 386}):ease('expoinout')
+        cursor.label = '>         <'
+        flux.to(start, EASE_TIME, {x = 512}):ease('expoinout')
+        flux.to(setting, EASE_TIME, {x = 512}):ease('expoinout')
+        flux.to(hanko, EASE_TIME, {x = 512}):ease('expoinout')
+        flux.to(onigo, EASE_TIME, {x = 512}):ease('expoinout')
+
+        for i = 1, #scores.x do
+            flux.to(scores.x, EASE_TIME * 0.8, {[i] = 1024 + 200 - (1024 + 400) * (i % 2)}):ease('expoinout'):delay((i) * 0.03)
         end
-        end
+    end
 end
 
 function ranking:printCenter(fontType, fontSize, char, x, y, scale)
--- state/title.lua
+++ state/title.lua
@@ -7,24 +7,22 @@ function title:init()
     self.cursorYMax = 3
     self.cursorDY = 70
     self.cursorNormY = 316
-
-
 end
 
 function title:enter()
     --self.fontTitle = love.graphics.newFont(64)
-    self.cursorChar = ">         <"
+    self.cursorChar = '>         <'
     self.pressZWaiting = 0
 
     hanko = {
         x = 512,
         y = 132,
-        label = "HANKO      "
+        label = 'HANKO      '
     }
     onigo = {
         x = 512,
         y = 132,
-        label = "      ONIGO"
+        label = '      ONIGO'
     }
 
     start = {
@@ -35,12 +33,12 @@ function title:enter()
 
     ranking = {
         x = 512,
-        y = self.cursorNormY+self.cursorDY*1,
+        y = self.cursorNormY + self.cursorDY * 1
     }
 
     setting = {
         x = 512,
-        y = self.cursorNormY+self.cursorDY*2,
+        y = self.cursorNormY + self.cursorDY * 2
     }
     self.keyManager = KeyManager()
     self.keyManager:register(
@@ -68,23 +66,48 @@ function title:enter()
                 end,
                 rep = false,
                 act = 'pressed'
-            },
+            }
         }
     )
-
+    self.joystickManager = JoystickManager()
+    self.joystickManager:register(
+        {
+            {
+                key = 'a',
+                func = function()
+                    self:pressZ()
+                end,
+                rep = false,
+                act = 'pressed'
+            },
+            {
+                key = 'lefty',
+                func = function(axisValue)
+                    if axisValue > 0 then
+                        self:moveCursor(-1)
+                    else
+                        self:moveCursor(1)
+                    end
+                end,
+                rep = false,
+                act = 'pressed'
+            }
+        }
+    )
 end
 
 function title:update(dt)
     self.keyManager:update(dt)
+    self.joystickManager:update(dt)
     flux.update(dt)
 
     -- Zが押されてから次のシーンに遷移するまでちょっと待つ
     if self.pressZWaiting >= 1 then
         self.pressZWaiting = self.pressZWaiting + 1
         if self.pressZWaiting % 30 > 15 then
-            self.cursorChar = ">         <"
+            self.cursorChar = '>         <'
         else
-            self.cursorChar = ">          <"
+            self.cursorChar = '>          <'
         end
 
         if self.pressZWaiting >= 90 then
@@ -99,13 +122,13 @@ function title:draw()
     love.graphics.setColor(1, 1, 1, 1)
     --love.graphics.setFont(Data.Font.title)
     --love.graphics.print("HANKO ONIGO", 512-5.5*32, 150)
-    self:printCenter(Data.Font.veryBig,Data.Font.size.veryBig,hanko.label, hanko.x, hanko.y,1)
-    self:printCenter(Data.Font.veryBig,Data.Font.size.veryBig,onigo.label, onigo.x, onigo.y,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"START", start.x, start.y,start.scale)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"RANKING", ranking.x, ranking.y,1)
-    self:printCenter(Data.Font.title,Data.Font.size.title,"SETTING", setting.x, setting.y,1)
+    self:printCenter(Data.Font.veryBig, Data.Font.size.veryBig, hanko.label, hanko.x, hanko.y, 1)
+    self:printCenter(Data.Font.veryBig, Data.Font.size.veryBig, onigo.label, onigo.x, onigo.y, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'START', start.x, start.y, start.scale)
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'RANKING', ranking.x, ranking.y, 1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, 'SETTING', setting.x, setting.y, 1)
     --self:printCenter(Data.Font.title,Data.Font.size.title,""..self.cursorY, 512, self.cursorNormY+self.cursorDY*3)
-    self:printCenter(Data.Font.title,Data.Font.size.title,self.cursorChar, 512, self.cursorNormY+self.cursorY*self.cursorDY,1)
+    self:printCenter(Data.Font.title, Data.Font.size.title, self.cursorChar, 512, self.cursorNormY + self.cursorY * self.cursorDY, 1)
 end
 
 function title:leave()
@@ -114,13 +137,13 @@ end
 function title:pressZ()
     if self.cursorY == 0 then
         self.pressZWaiting = 1
-        flux.to(start,0.5,{scale = 1.5}):ease("elasticout")
-        flux.to(hanko,0.5,{x=-200}):ease("expoin")
-        flux.to(onigo,0.5,{x=1024+200}):ease("expoin")
-        flux.to(ranking,0.5,{x=-200}):ease("expoin")
-        flux.to(setting,0.5,{x=1024+200}):ease("expoin")
-        
-        --State.switch(States.Maingame)
+        flux.to(start, 0.5, {scale = 1.5}):ease('elasticout')
+        flux.to(hanko, 0.5, {x = -200}):ease('expoin')
+        flux.to(onigo, 0.5, {x = 1024 + 200}):ease('expoin')
+        flux.to(ranking, 0.5, {x = -200}):ease('expoin')
+        flux.to(setting, 0.5, {x = 1024 + 200}):ease('expoin')
+
+    --State.switch(States.Maingame)
     end
     if self.cursorY == 1 then
         State.switch(States.Ranking)
@@ -133,13 +156,13 @@ end
 function title:moveCursor(dy)
     if self.pressZWaiting == 0 then
         --self.cursorY = math.max(math.min(self.cursorY+dy,2),0)
-        flux.to(self,0.15,{cursorY = math.max(math.min(self.cursorY+dy,2),0)}):ease("expoout")
+        flux.to(self, 0.15, {cursorY = math.max(math.min(self.cursorY + dy, 2), 0)}):ease('expoout')
     end
 end
 
-function title:printCenter(fontType,fontSize,char,x,y,scale)
+function title:printCenter(fontType, fontSize, char, x, y, scale)
     love.graphics.setFont(fontType)
-    love.graphics.print(char, x-(#char/2)*fontSize/2*scale, y-fontSize/2*scale,0,scale)
+    love.graphics.print(char, x - (#char / 2) * fontSize / 2 * scale, y - fontSize / 2 * scale, 0, scale)
 end
 
 return title
